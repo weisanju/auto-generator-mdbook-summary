98fe3c938e660b97af75c21551078c79866ed267
肖佳权 <xiaojiaquanjiaquan@alibaba-inc.com>
2022-06-20 01:16:47 +0800

    feat: rebuild FirstDir Logic-2

--- src/main/java/com/weisanju/autoGeneratorSummary/core/FirstFileObj.java
+++ src/main/java/com/weisanju/autoGeneratorSummary/core/FirstFileObj.java
@@ -4,44 +4,40 @@ import lombok.Data;
 
 import java.io.File;
 import java.util.Arrays;
+
 @Data
-public class FirstFileObj extends FileObj implements DirFileObj{
+public class FirstFileObj extends FileObj implements DirFileObj {
     protected String groupName = "";
     protected int order;
     protected String contentName;
 
-    public FirstFileObj(File realFile,FileObj parent) {
-        super(realFile,parent);
-
-
-        //解析其他
+    public FirstFileObj(File realFile, FileObj parent) {
+        super(realFile, parent);
         String name = realFile.getName();
-        int i1 = name.indexOf('_');
-        //没有则取默认分组为空字符串
-        if (i1 < 0) {
-            contentName = name;
-            return;
-        }
         int i2 = name.indexOf('.');
-        if (i2 < 0) {
-            groupName = name.substring(0, i1);
+        if (i2 > -1) {
+            try {
+                order = Integer.parseInt(name.substring(0, i2));
+            } catch (NumberFormatException ignored) {
+            }
+            name = name.substring(i2 + 1);
+        }
+        int i1 = name.indexOf('_');
+        if (i1 > -1) {
             contentName = name.substring(i1 + 1);
-            return;
+            groupName = name.substring(0, i1);
+        } else {
+            contentName = name;
         }
-        groupName = name.substring(i2+1,i1);
-        try {
-            order = Integer.parseInt(name.substring(0,i2));
-        } catch (NumberFormatException ignored) {
-        }
-        contentName = name.substring(i1 + 1);
+
     }
 
     @Override
     String write(int depth) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(repeatString("  ",depth+1,"")).append("* [").append(getContentName()).append("](").append(getReadMeFile()).append(")").append(System.lineSeparator());
+        stringBuilder.append(repeatString("  ", depth + 1, "")).append("* [").append(getContentName()).append("](").append(getReadMeFile()).append(")").append(System.lineSeparator());
         for (FileObj child : children) {
-            stringBuilder.append(child.write(depth+1));
+            stringBuilder.append(child.write(depth + 1));
         }
         return stringBuilder.toString();
     }
